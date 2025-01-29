96a3b1ea39276357e42bfcca9607c309b70bf826
Fidelisaboke <fidelisaboke@gmail.com>
2025-01-29 12:00:33 +0300

    feat: remove status parameter from __init__ - The status will be set to 'to-do' by default

--- task.py
+++ task.py
@@ -1,15 +1,12 @@
 from datetime import datetime
 
 class Task:
-    def __init__(self, id: int, description: str, status):
+    def __init__(self, id: int, description: str):
         current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         self.id = id
         self.description = description
-        self.status = status
+        self.status = "to-do"
         self.created_at = current_datetime
         self.updated_at = current_datetime
 
-
-task = Task(1, "A task", "to-do")
-print(vars(task))
