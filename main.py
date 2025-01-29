ca8a844deb79897916a5cb218a9e8187b20b052e
Fidelisaboke <fidelisaboke@gmail.com>
2025-01-29 12:40:32 +0300

    feat: add update tasks functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import logging
 from functions import create_parser, get_data
 from task import Task
+from datetime import datetime
 
 # Tasks file
 TASKS_FILE = "tasks.json"
@@ -34,3 +35,33 @@ if args.action == "add":
         file.write(serialized_data)
 
         print(f"Task added successfully (ID: {task['id']})")
+
+
+# Update an existing task
+elif args.action == "update":
+    tasks = get_data(TASKS_FILE)
+    print(tasks)        
+
+    for task in tasks:
+
+        # Update task if it exists
+        if task['id'] == args.id:
+            task['description'] = args.description
+            task['updated_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            break
+
+    else:
+        print(f"Task does not exist (ID: {args.id})")
+        raise SystemExit(1)
+
+    # Save the tasks to the tasks file
+    with open(TASKS_FILE, "w") as file:
+        tasks.append(task)
+        serialized_tasks_data = json.dumps(tasks, indent=4)
+        file.write(serialized_tasks_data)
+
+        print("Task updated successfully.")
+
+    
+    
+
