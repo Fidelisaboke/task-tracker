7f10b1efacf9291cd82e1d73e5274c903a9280d8
Fidelisaboke <fidelisaboke@gmail.com>
2025-01-29 16:02:16 +0300

    style: reformat functions for code readability

--- functions.py
+++ functions.py
@@ -18,28 +18,52 @@ def create_parser():
 
     # Add a task
     add_task_subparser = subparsers.add_parser("add", help="Add a new task.")
-    add_task_subparser.add_argument("description", type=str, help="Task description")
+    add_task_subparser.add_argument(
+        "description", 
+        type=str, 
+        help="Task description"
+    )
 
     # Update a task
-    update_task_subparser = subparsers.add_parser("update", help="Update an existing task.")
+    update_task_subparser = subparsers.add_parser(
+        "update", 
+        help="Update an existing task."
+    )
     update_task_subparser.add_argument("id", type=int, help="Task id")
-    update_task_subparser.add_argument("description", type=str, help="Task description")
+    update_task_subparser.add_argument(
+        "description", 
+        type=str, 
+        help="Task description"
+    )
 
     # Delete a task
-    delete_task_subparser = subparsers.add_parser("delete", help="Delete an existing task.")
+    delete_task_subparser = subparsers.add_parser(
+        "delete", 
+        help="Delete an existing task."
+    )
     delete_task_subparser.add_argument("id", type=int, help="Task id")
 
     # Mark a task as in progress
-    in_progress_subparser = subparsers.add_parser("mark-in-progress", help="Mark a task as in progress.")
+    in_progress_subparser = subparsers.add_parser(
+        "mark-in-progress", 
+        help="Mark a task as in progress."
+    )
     in_progress_subparser.add_argument("id", type=int, help="Task id")
 
     # Mark a task as done
-    task_done_subparser = subparsers.add_parser("mark-done", help="Mark a task as done.")
+    task_done_subparser = subparsers.add_parser(
+        "mark-done", 
+        help="Mark a task as done."
+    )
     task_done_subparser.add_argument("id", type=int, help="Task id")
 
     # Listing tasks
     list_subparser = subparsers.add_parser("list", help="List all tasks.")
-    list_subparser.add_argument("status", help="The task status.", action="store_true")
+    list_subparser.add_argument(
+        "status", 
+        help="The task status.", 
+        action="store_true"
+    )
 
     return parser
 
